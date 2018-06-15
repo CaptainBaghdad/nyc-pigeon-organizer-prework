b84f9491672b9c418115c26b10435de66efeb3e5
Jason Pitts <juniorjr083@gmail.com>
2018-06-15 13:37:49 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   arr =[]
- data.select do |k,v|
+ ans = data.select do |k,v|
      v.collect do |kk,vv|
      puts  vv.uniq 
       
@@ -10,5 +10,5 @@ def nyc_pigeon_organizer(data)
         
      end 
   end
-   
+   keys_arr = ans.keys.uniq
 end
