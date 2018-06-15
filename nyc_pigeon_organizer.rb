4b3ad456e69560d956048837701577d9d2c78bea
Jason Pitts <juniorjr083@gmail.com>
2018-06-15 13:58:11 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,10 +9,10 @@ def nyc_pigeon_organizer(data)
   end
    name_keys = arr.flatten.uniq
    name_keys.each{|ele| new_hash[ele] = {}}
-   new_hash
-   data.each do |k,v|  
-   new_hash.each do |kk,vv|
-     new_hash[kk] = k
+  
+  new_hash.each do |k,v|  
+   data.each do |kk,vv|
+     new_hash[k] = kk
    end 
    end
   new_hash 
